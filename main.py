226482a5971bd3b287725924c5288c2c53987ccd
MangoJuiceee <72989195+MangoJuiceee@users.noreply.github.com>
2021-06-29 15:00:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/main")
-def mainpage() -> str:
+def mainpage() -> "html":
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    """Принимает картинку"""
+    pic = request.files["image"]
+
+    if not pic:
+        return "Изображение не выбрано", 400
+    
+    else:
+        filename = secure_filename(pic.filename)
+        mimetype = pic.mimetype
 
 
 if __name__ == "__main__":
