22b99aa71666e7838dc3cf336de636463058b32d
MangoJuiceee <72989195+MangoJuiceee@users.noreply.github.com>
2021-06-29 18:28:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request
+
+import os
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
+from PIL import Image
 
 app = Flask(__name__)
+
+dir_path = "Images/"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -18,8 +28,17 @@ def upload():
     if not pic:
         return "Изображение не выбрано", 400
     
+    #Настроить проверку формата файла
+    #elif allowed_file(pic) != True:
+    #   
+    #   return redirect()
+
+    #сохранить файлы в определённой папке
     else:
-        return "Загружено"
+        img = Image.open(pic)
+        img.save("Images/pic.JPEG")
+
+    return render_template("upload.html")
 
 
 if __name__ == "__main__":
